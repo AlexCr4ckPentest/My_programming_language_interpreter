fce0170e93241dfd17bd80ec4d86dfbc19c3e207
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-07-06 17:14:02 +0300

    add parser class

---
+++ include/parser.h
@@ -0,0 +1,23 @@
+#include <vector>
+#include <stack>
+#include <memory>
+
+#include "token.h"
+#include "abstract_expression.h"
+
+#ifndef PARSER_H
+#define PARSER_H
+
+class Parser
+{
+public:
+    inline explicit Parser(const std::vector<Token>& tokens_list) noexcept
+        : m_tokens_list {tokens_list}
+    {}
+
+private:
+    const std::vector<Token> m_tokens_list {};
+    std::stack<std::unique_ptr<AbstractExpression> > m_expression_ptrs {};
+};
+
+#endif // PARSER_H
