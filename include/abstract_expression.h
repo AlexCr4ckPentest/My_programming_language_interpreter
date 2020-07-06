4eea4e081393f1d08b13152868465ce4db423dc8
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-07-06 17:14:39 +0300

    add abstract class AbstractExpression

---
+++ include/abstract_expression.h
@@ -0,0 +1,10 @@
+#ifndef ABSTRACT_EXPRESSION_H
+#define ABSTRACT_EXPRESSION_H
+
+class AbstractExpression
+{
+public:
+    virtual long double eval() noexcept = 0;
+};
+
+#endif // ABSTRACT_EXPRESSION_H
