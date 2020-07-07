077ec1d5eb3b34ea7209430ec45c7dc8d98a83e6
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-07-07 11:37:07 +0300

    added implementation of expression parsers

--- include/parser.h
+++ include/parser.h
@@ -1,9 +1,13 @@
 #include <vector>
 #include <stack>
 #include <memory>
+#include <stdexcept>
 
 #include "token.h"
+
 #include "expressions/abstract_expression.h"
+#include "expressions/number_expression.h"
+#include "expressions/binary_expression.h"
 
 #ifndef PARSER_H
 #define PARSER_H
@@ -15,11 +19,35 @@ public:
         : m_tokens_list {tokens_list}
     {}
 
-    void parse() noexcept;
+    Parser(const Parser&) = delete;
+    Parser& operator =(const Parser&) = delete;
+
+    std::vector<std::shared_ptr<AbstractExpression> > parse() noexcept;
+
+    Token peek(const size_t at_index) const noexcept;
+    inline Token nextToken() noexcept
+    {
+        m_current_position++;
+        return peek(0);
+    }
+
+    bool match(const TokenType token_type) noexcept;
+
+    inline std::shared_ptr<AbstractExpression> expression()
+    { return additive(); }
+
+    std::shared_ptr<AbstractExpression> primary();
+    std::shared_ptr<AbstractExpression> unary();
+
+    std::shared_ptr<AbstractExpression> additive();
+    std::shared_ptr<AbstractExpression> multiplicative();
 
 private:
+    const Token EOF_token {TokenType::END_OF_FILE};
+
     const std::vector<Token> m_tokens_list {};
-    std::stack<std::unique_ptr<AbstractExpression> > m_expression_ptrs {};
+    std::stack<std::shared_ptr<AbstractExpression> > m_expression_ptrs {};
+    size_t m_current_position {};
 };
 
 #endif // PARSER_H
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,96 @@
 #include "../include/parser.h"
 
-void Parser::parse() noexcept
+Token Parser::peek(const size_t at_index) const noexcept
 {
+    const size_t position {m_current_position + at_index};
 
+    if (position >= static_cast<size_t>(m_tokens_list.size())) {
+        return EOF_token;
+    }
+
+    return m_tokens_list.at(position);
+}
+
+
+
+bool Parser::match(const TokenType token_type) noexcept
+{
+    const TokenType current_token_type {peek(0).getType()};
+
+    if (token_type != current_token_type) {
+        return false;
+    }
+
+    m_current_position++;
+    return true;
+}
+
+
+
+std::shared_ptr<AbstractExpression> Parser::primary()
+{
+    const Token current_token {peek(0)};
+
+    if (match(TokenType::NUMBER)) {
+        return std::make_shared<AbstractExpression>(NumberExpression(std::stold(current_token.getData())));
+    }
+
+    throw std::runtime_error("Error: unknown token type!\n");
+}
+
+
+
+std::shared_ptr<AbstractExpression> Parser::unary()
+{
+    const Token current_token {peek(0)};
+
+    if (match(TokenType::OPERATOR_SUB)) {
+        return std::make_shared<AbstractExpression>(NumberExpression{std::stold(current_token.getData())});
+    }
+
+    return primary();
+}
+
+
+
+std::shared_ptr<AbstractExpression> Parser::additive()
+{
+    std::shared_ptr<AbstractExpression> left_expr {multiplicative()};
+    std::shared_ptr<AbstractExpression> result {};
+
+    while (true) {
+        if (match(TokenType::OPERATOR_ADD)) {
+            result = std::make_shared<AbstractExpression>(BinaryExpression(left_expr, multiplicative(), '+'));
+        }
+        else if (match(TokenType::OPERATOR_SUB)) {
+            result = std::make_shared<AbstractExpression>(BinaryExpression(left_expr, multiplicative(), '-'));
+        }
+        else {
+            break;
+        }
+    }
+
+    return result;
+}
+
+
+
+std::shared_ptr<AbstractExpression> Parser::multiplicative()
+{
+    std::shared_ptr<AbstractExpression> left_expr {unary()};
+    std::shared_ptr<AbstractExpression> result {};
+
+    while (true) {
+        if (match(TokenType::OPERATOR_MUL)) {
+            result = std::make_shared<AbstractExpression>(BinaryExpression(left_expr, unary(), '*'));
+        }
+        else if (match(TokenType::OPERATOR_DIV)) {
+            result = std::make_shared<AbstractExpression>(BinaryExpression(left_expr, unary(), '/'));
+        }
+        else {
+            break;
+        }
+    }
+
+    return result;
 }
